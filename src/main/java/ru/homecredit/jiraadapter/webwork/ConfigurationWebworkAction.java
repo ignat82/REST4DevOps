eafdc14b5dd89ffe6c93f644609de76cc0ec1b4a
ignat82 <ignat.nushtaev@outlook.com>
2022-09-25 16:58:54 +0300

    removed logs

--- src/main/java/ru/homecredit/jiraadapter/webwork/ConfigurationWebworkAction.java
+++ src/main/java/ru/homecredit/jiraadapter/webwork/ConfigurationWebworkAction.java
@@ -2,17 +2,18 @@ package ru.homecredit.jiraadapter.webwork;
 
 import com.atlassian.jira.web.action.JiraWebActionSupport;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import ru.homecredit.jiraadapter.service.JiraAdapterSettingsService;
 
-import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @Getter
 @Setter
+@RequiredArgsConstructor
 /**
  * webwork class to output configuration page template and exchange plugin settings string with it
  */
@@ -22,37 +23,21 @@ public class ConfigurationWebworkAction extends JiraWebActionSupport {
     private List<String> savedCustomFieldsKeys;
     private String[] customFieldsKeysToSave;
 
-    @Inject
-    /**
-     * constructor to acquire JiraSettingsService bean and receive current settings from jira
-     */
-    public ConfigurationWebworkAction(JiraAdapterSettingsService jiraAdapterSettingsService) {
-
-        log.info("constructing");
-        this.jiraAdapterSettingsService = jiraAdapterSettingsService;
-    }
-
     @Override
     /**
      * method either saves settings received from template in customFieldsIds field,
      * or just returns the template, if no POST request was preformed
      */
     public String execute() throws Exception {
-        log.info("execute()");
-        log.info("super.execute()");
         super.execute();
-        log.info("preparing fields info");
         allCustomFieldsKeys = jiraAdapterSettingsService.getAllSelectListFieldsKeys();
         savedCustomFieldsKeys = jiraAdapterSettingsService.getSettings().getEditableFields();
-        log.info("returning template from execute()");
         return "configuration-page";
     }
 
     public void doSave() {
-        log.info("doSave()");
         log.info("saving {}", Arrays.toString(customFieldsKeysToSave));
         jiraAdapterSettingsService.saveCustomFieldsKeys(customFieldsKeysToSave);
-        log.info("returning from doSave()");
     }
 
 }
