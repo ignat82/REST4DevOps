879cadb6421ffa09a14790bdc4f6b83624376514
ignat82 <ignat.nushtaev@outlook.com>
2022-11-22 14:35:17 +0300

    refactoring

--- src/main/java/ru/homecredit/jiraadapter/service/FieldInitializationService.java
+++ src/main/java/ru/homecredit/jiraadapter/service/FieldInitializationService.java
@@ -1,10 +1,108 @@
 package ru.homecredit.jiraadapter.service;
 
+import com.atlassian.jira.issue.context.IssueContext;
+import com.atlassian.jira.issue.context.IssueContextImpl;
+import com.atlassian.jira.issue.customfields.manager.OptionsManager;
+import com.atlassian.jira.issue.customfields.option.Option;
+import com.atlassian.jira.issue.customfields.option.Options;
+import com.atlassian.jira.issue.fields.ConfigurableField;
+import com.atlassian.jira.issue.fields.FieldManager;
+import com.atlassian.jira.issue.fields.config.FieldConfig;
+import com.atlassian.jira.project.Project;
+import com.atlassian.jira.project.ProjectManager;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import lombok.extern.slf4j.Slf4j;
+import ru.homecredit.jiraadapter.dto.FieldOptions;
+import ru.homecredit.jiraadapter.dto.FieldParameters;
+import ru.homecredit.jiraadapter.dto.JiraAdapterSettings;
+import ru.homecredit.jiraadapter.dto.request.FieldOptionsRequest;
 
+import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Named
 public class FieldInitializationService {
+    private final FieldManager fieldManager;
+    private final ProjectManager projectManager;
+    private final OptionsManager optionsManager;
+    private final JiraAdapterSettingsService jiraAdapterSettingsService;
+
+    @Inject
+    public FieldInitializationService(@ComponentImport FieldManager fieldManager,
+                                      @ComponentImport ProjectManager projectManager,
+                                      @ComponentImport OptionsManager optionsManager,
+                                      JiraAdapterSettingsServiceImpl jiraAdapterSettingsService) {
+        this.fieldManager = fieldManager;
+        this.projectManager = projectManager;
+        this.optionsManager = optionsManager;
+        this.jiraAdapterSettingsService = jiraAdapterSettingsService;
+    }
+
+    /**
+     * method to acquire the options of given field in given context
+     * @param fieldOptionsRequest - DTO with request parameters
+     * @return - FieldOptions transport object
+     */
+    FieldOptions initializeField(FieldOptionsRequest fieldOptionsRequest) {
+        FieldOptions fieldOptions = new FieldOptions(fieldOptionsRequest);
+        fieldOptions.setFieldParameters(initializeFieldParameters(fieldOptionsRequest));
+        if (fieldOptions.getErrorMessage() == null) {
+            initializeOptions(fieldOptions);
+        }
+        return fieldOptions;
+    }
+
+    /**
+     *
+     * @param fieldOptionsRequest - DTO, containing request parameters
+     * @return - FieldParameters DTO with some Jira properties of manipulated customfield
+     */
+    private FieldParameters initializeFieldParameters(FieldOptionsRequest fieldOptionsRequest) {
+        FieldParameters fieldParameters = new FieldParameters();
+        ConfigurableField field =
+                fieldManager.getConfigurableField(fieldOptionsRequest.getFieldKey());
+        if (field == null) {
+            log.error("failed acquire field object");
+            return null;
+        }
+        fieldParameters.setFieldName(field.getName());
+        Project project = projectManager.
+                getProjectByCurrentKeyIgnoreCase(fieldOptionsRequest.getProjectKey());
+        fieldParameters.setProjectName(project.getName());
+        IssueContext issueContext =
+                new IssueContextImpl(project.getId(), fieldOptionsRequest.getIssueTypeId());
+        FieldConfig fieldConfig = field.getRelevantConfig(issueContext);
+        fieldParameters.setFieldConfig(fieldConfig);
+        fieldParameters.setFieldConfigName(fieldConfig.getName());
+        fieldParameters.setValidContext(true);
+        log.trace("valid context - " + fieldParameters.isValidContext());
+        JiraAdapterSettings jiraAdapterSettings = jiraAdapterSettingsService.getSettings();
+        List<String> editableFields = jiraAdapterSettings.getEditableFields();
+        log.trace("editable fields are - {}", editableFields);
+        boolean permittedToEdit = editableFields != null &&
+                editableFields.contains(fieldOptionsRequest.getFieldKey());
+        log.trace("field {} is permitted to edit - {}", field.getName(), permittedToEdit);
+        fieldParameters.setPermittedToEdit(permittedToEdit);
+        return fieldParameters;
+    }
+
+    /**
+     * method to initialize options of field, attributes of which are stored in
+     * manipulated FieldOptions DTO
+     * @param fieldOptions - transport object
+     */
+    // side effect
+    void initializeOptions(FieldOptions fieldOptions) {
+        Options options = Objects.requireNonNull(optionsManager.
+                                                         getOptions(fieldOptions.getFieldParameters().getFieldConfig()),
+                                                 "failed to acquire Options object");
+        fieldOptions.setFieldOptionsList(options.stream().map(Option::getValue).collect(Collectors.toList()));
+        fieldOptions.setJiraOptions(options.stream().map(FieldOptions.JiraOption::new).collect(Collectors.toList()));
+        log.trace("field options id's are {}", fieldOptions.getJiraOptions().toString());
+        log.trace("field options are {}", fieldOptions.getFieldOptionsList().toString());
+    }
 }
--- src/main/java/ru/homecredit/jiraadapter/service/FieldOptionsServiceImpl.java
+++ src/main/java/ru/homecredit/jiraadapter/service/FieldOptionsServiceImpl.java
@@ -1,55 +1,38 @@
 package ru.homecredit.jiraadapter.service;
 
-import com.atlassian.jira.issue.context.IssueContext;
-import com.atlassian.jira.issue.context.IssueContextImpl;
 import com.atlassian.jira.issue.customfields.manager.OptionsManager;
 import com.atlassian.jira.issue.customfields.option.Option;
-import com.atlassian.jira.issue.customfields.option.Options;
-import com.atlassian.jira.issue.fields.ConfigurableField;
-import com.atlassian.jira.issue.fields.FieldManager;
-import com.atlassian.jira.issue.fields.config.FieldConfig;
-import com.atlassian.jira.project.Project;
-import com.atlassian.jira.project.ProjectManager;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import lombok.extern.slf4j.Slf4j;
 import ru.homecredit.jiraadapter.dto.FieldOptions;
-import ru.homecredit.jiraadapter.dto.FieldParameters;
-import ru.homecredit.jiraadapter.dto.JiraAdapterSettings;
 import ru.homecredit.jiraadapter.dto.request.FieldOptionsRequest;
 
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.Collections;
 
 import static ru.homecredit.jiraadapter.dto.Constants.DEFAULT_RECEIVED;
-import static ru.homecredit.jiraadapter.dto.FieldOptions.JiraOption;
 import static ru.homecredit.jiraadapter.dto.request.FieldOptionsRequest.Action;
 import static ru.homecredit.jiraadapter.dto.request.FieldOptionsRequest.Action.*;
 
 @Slf4j
 @Named
 public class FieldOptionsServiceImpl implements FieldOptionsService {
-    private final FieldManager fieldManager;
-    private final ProjectManager projectManager;
     private final OptionsManager optionsManager;
-    private final JiraAdapterSettingsService jiraAdapterSettingsService;
+    private final FieldInitializationService fieldInitializationService;
 
     @Inject
-    public FieldOptionsServiceImpl(@ComponentImport FieldManager fieldManager,
-                                   @ComponentImport ProjectManager projectManager,
-                                   @ComponentImport OptionsManager optionsManager,
-                                   JiraAdapterSettingsServiceImpl jiraAdapterSettingsService) {
-        this.fieldManager = fieldManager;
-        this.projectManager = projectManager;
+    public FieldOptionsServiceImpl(@ComponentImport OptionsManager optionsManager,
+                                   FieldInitializationService fieldInitializationService) {
         this.optionsManager = optionsManager;
-        this.jiraAdapterSettingsService = jiraAdapterSettingsService;
+        this.fieldInitializationService = fieldInitializationService;
     }
     public FieldOptions getOptions(String fieldKey,
                                    String projectKey,
                                    String issueTypeId) {
-        FieldOptions fieldOptions
-                = initializeField(new FieldOptionsRequest(fieldKey, projectKey, issueTypeId));
+        FieldOptions fieldOptions = fieldInitializationService
+                .initializeField(new FieldOptionsRequest(fieldKey, projectKey, issueTypeId));
         if (fieldOptions.getErrorMessage() == null) {
             fieldOptions.setSuccess(true);
         }
@@ -66,7 +49,7 @@ public class FieldOptionsServiceImpl implements FieldOptionsService {
             log.error(errorMessage);
             return fieldOptions;
         }
-        FieldOptions fieldOptions = initializeField(fieldOptionsRequest);
+        FieldOptions fieldOptions = fieldInitializationService.initializeField(fieldOptionsRequest);
         if (fieldOptions.getErrorMessage() != null) {
             log.error("shutting down postOption() due to error");
             return fieldOptions;
@@ -111,7 +94,7 @@ public class FieldOptionsServiceImpl implements FieldOptionsService {
         log.trace("renamed option from \"{}\"  to \"{}\"", oldOptionValue, newOptionValue);
         /* acquiring Options object and Options from it once again, cuz the
         option state changed */
-        initializeOptions(fieldOptions);
+        fieldInitializationService.initializeOptions(fieldOptions);
         return fieldOptions;
     }
     private FieldOptions addOption(FieldOptions fieldOptions) {
@@ -132,7 +115,7 @@ public class FieldOptionsServiceImpl implements FieldOptionsService {
         log.trace("added option \"{}\" to Options", optionValue);
         /* acquiring Options object and Options from it once again, cuz the
         new one was appended */
-        initializeOptions(fieldOptions);
+        fieldInitializationService.initializeOptions(fieldOptions);
         return fieldOptions;
     }
 
@@ -144,72 +127,8 @@ public class FieldOptionsServiceImpl implements FieldOptionsService {
         log.trace("set option \"{}\" isDisabled state to {}", option.getValue(), isDisabled);
         /* acquiring Options object and Options from it once again, cuz the
         option state changed */
-        initializeOptions(fieldOptions);
+        fieldInitializationService.initializeOptions(fieldOptions);
         return fieldOptions;
-    }
-
-    /**
-     * method to acquire the options of given field in given context
-     * @param fieldOptionsRequest - DTO with request parameters
-     * @return - FieldOptions transport object
-     */
-    private FieldOptions initializeField(FieldOptionsRequest fieldOptionsRequest) {
-        FieldOptions fieldOptions = new FieldOptions(fieldOptionsRequest);
-        fieldOptions.setFieldParameters(initializeFieldParameters(fieldOptionsRequest));
-        if (fieldOptions.getErrorMessage() == null) {
-            initializeOptions(fieldOptions);
-        }
-        return fieldOptions;
-    }
-
-    /**
-     *
-     * @param fieldOptionsRequest - DTO, containing request parameters
-     * @return - FieldParameters DTO with some Jira properties of manipulated customfield
-     */
-    private FieldParameters initializeFieldParameters(FieldOptionsRequest fieldOptionsRequest) {
-        FieldParameters fieldParameters = new FieldParameters();
-        ConfigurableField field =
-                fieldManager.getConfigurableField(fieldOptionsRequest.getFieldKey());
-        if (field == null) {
-            log.error("failed acquire field object");
-            return null;
-        }
-        fieldParameters.setFieldName(field.getName());
-        Project project = projectManager.
-              getProjectByCurrentKeyIgnoreCase(fieldOptionsRequest.getProjectKey());
-        fieldParameters.setProjectName(project.getName());
-        IssueContext issueContext =
-                new IssueContextImpl(project.getId(), fieldOptionsRequest.getIssueTypeId());
-        FieldConfig fieldConfig = field.getRelevantConfig(issueContext);
-        fieldParameters.setFieldConfig(fieldConfig);
-        fieldParameters.setFieldConfigName(fieldConfig.getName());
-        fieldParameters.setValidContext(true);
-        log.trace("valid context - " + fieldParameters.isValidContext());
-        JiraAdapterSettings jiraAdapterSettings = jiraAdapterSettingsService.getSettings();
-        List<String> editableFields = jiraAdapterSettings.getEditableFields();
-        log.trace("editable fields are - {}", editableFields);
-        boolean permittedToEdit = editableFields != null &&
-                editableFields.contains(fieldOptionsRequest.getFieldKey());
-        log.trace("field {} is permitted to edit - {}", field.getName(), permittedToEdit);
-        fieldParameters.setPermittedToEdit(permittedToEdit);
-        return fieldParameters;
-    }
-
-    /**
-     * method to initialize options of field, attributes of which are stored in
-     * manipulated FieldOptions DTO
-     * @param fieldOptions - transport object
-     */
-    // side effect
-    private void initializeOptions(FieldOptions fieldOptions) {
-        Options options = Objects.requireNonNull(optionsManager.
-              getOptions(fieldOptions.getFieldParameters().getFieldConfig()),
-              "failed to acquire Options object");
-        fieldOptions.setFieldOptionsList(options.stream().map(Option::getValue).collect(Collectors.toList()));
-        fieldOptions.setJiraOptions(options.stream().map(JiraOption::new).collect(Collectors.toList()));
-        log.trace("field options id's are {}", fieldOptions.getJiraOptions().toString());
-        log.trace("field options are {}", fieldOptions.getFieldOptionsList().toString());
     }
 
 }
